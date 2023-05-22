87179ccdcac6a508796d0f374e023280c6e2c739
jessres <127244776+jessres@users.noreply.github.com>
2023-05-22 17:05:20 -0500

    Update qc_report.sh
    Added new header and replaced spaces with underscores

--- qc_report.sh
+++ qc_report.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-version="qc_report.sh version 1.0"
+version="version 1.0"
 #author		 :Jessica Respress
 #date		   :20230517
 #usage		 :bash qc_report.sh <run_name>
 
-echo "Running qc_report.sh version %s" >> $run_dir/output/$1/mycosnptx.log
 run_dir=$PWD/mycosnp-nf
+echo "Running qc_report.sh $version" >> $run_dir/output/$1/mycosnptx.log
+
 
 awk 'BEGIN {FS="\t"};\
 	NR==1{$14="Pass/Fail";print;next};\
 	$9 > 42 && $9 < 47.5 && $10 >= 28 && $11 >= 20{printf "%s\t%s\n", $0, "PASS"};\
-	$9 < 42 || $9 > 47.5 || $10 < 28 || $11 < 20{printf "%s\t%s\n", $0, "FAIL"};' $run_dir/output/$1/stats/qc_report/qc_report.txt > $run_dir/output/$1/$1"_QCREPORT.txt"
+	$9 < 42 || $9 > 47.5 || $10 < 28 || $11 < 20{printf "%s\t%s\n", $0, "FAIL"};' $run_dir/output/$1/stats/qc_report/qc_report.txt > $run_dir/output/$1/$1"_QCREPORT.temp"
+ 
+awk '(NR>1)' $run_dir/output/$1/$1"_QCREPORT.temp" > $run_dir/output/$1/$1"_QCREPORT2.temp" 
+
+echo "Sample_Name	Reads_Before_Trimming	GC_Before_Trimming	Average_Q_Score_Before_Trimming	Reference_Length_Coverage_Before_Trimming	Reads_After_Trimming	Paired_Reads_After_Trimming	Unpaired_Reads_After_Trimming	GC_After_Trimming	Average_Q_Score_After_Trimming	Reference_Length_Coverage_After_Trimming	Mean_Coverage_Depth	Reads_Mapped	Pass/Fail" | cat - $run_dir/output/$1/$1"_QCREPORT2.temp" > $run_dir/output/$1/$1"_QCREPORT.text"
+
+rm $run_dir/output/$1/$1"_QCREPORT.temp"
+rm $run_dir/output/$1/$1"_QCREPORT2.temp"
