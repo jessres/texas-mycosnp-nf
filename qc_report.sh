6092fe545820cf05a3933d20c77b1ee3c0a103d9
jessres <127244776+jessres@users.noreply.github.com>
2023-07-11 10:59:52 -0500

    Update qc_report.sh
    awk command to read mycosnp-nf report output and flag sample if it does not meet CDC recommended QC parameters

--- qc_report.sh
+++ qc_report.sh
@@ -15,7 +15,16 @@ awk 'BEGIN {FS="\t"};\
  
 awk '(NR>1)' $run_dir/output/$1/$1"_QCREPORT.temp" > $run_dir/output/$1/$1"_QCREPORT2.temp" 
 
-echo "Sample_Name	Reads_Before_Trimming	GC_Before_Trimming	Average_Q_Score_Before_Trimming	Reference_Length_Coverage_Before_Trimming	Reads_After_Trimming	Paired_Reads_After_Trimming	Unpaired_Reads_After_Trimming	GC_After_Trimming	Average_Q_Score_After_Trimming	Reference_Length_Coverage_After_Trimming	Mean_Coverage_Depth	Reads_Mapped	Pass/Fail" | cat - $run_dir/output/$1/$1"_QCREPORT2.temp" > $run_dir/output/$1/$1"_QCREPORT.txt"
+awk 'BEGIN {FS="\t"};\
+  NR==2
+	$9 > 42 && $9 < 47.5 && $10 >= 28 && $11 >= 20{printf "%s\t%s\n", $0, " "};\
+ 	$9 < 42 || $9 > 47.5{printf "%s\t%s\n", $0, "GC_After_Trimming not within 42%-47.5%"};\
+ 	$10 < 28{printf "%s\t%s\n", $0, "Average_Q_Score_After_Trimming not greater than or equal to 28"};\
+	$11 < 20{printf "%s\t%s\n", $0, "Reference_Length_Coverage_After_Trimming not greater than or equal to 20X"};' $run_dir/output/$1/$1"_QCREPORT2.temp" > $run_dir/output/$1/$1"_QCREPORT3.temp"
+
+echo "Sample_Name	Reads_Before_Trimming	GC_Before_Trimming	Average_Q_Score_Before_Trimming	Reference_Length_Coverage_Before_Trimming	Reads_After_Trimming	Paired_Reads_After_Trimming	Unpaired_Reads_After_Trimming	GC_After_Trimming	Average_Q_Score_After_Trimming	Reference_Length_Coverage_After_Trimming	Mean_Coverage_Depth	Reads_Mapped	Pass/Fail  Comments" | cat - $run_dir/output/$1/$1"_QCREPORT3.temp" > $run_dir/output/$1/$1"_QCREPORT.txt"
+
 
 rm $run_dir/output/$1/$1"_QCREPORT.temp"
 rm $run_dir/output/$1/$1"_QCREPORT2.temp"
+rm $run_dir/output/$1/$1"_QCREPORT3.temp"
