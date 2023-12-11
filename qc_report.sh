40c15ee553e559a30a838a4ca2c024ca8a9dc208
jessres <127244776+jessres@users.noreply.github.com>
2023-12-11 09:17:33 -0600

    Update qc_report.sh

--- qc_report.sh
+++ qc_report.sh
@@ -7,24 +7,26 @@ version="version 1.0"
 run_dir=$PWD/mycosnp-nf
 echo "Running qc_report.sh $version" >> $run_dir/output/$1/mycosnptx.log
 
-
+awk '(NR>1)' $run_dir/output/$1/stats/qc_report/qc_report.txt > $run_dir/output/$1/$1"_QCREPORT.temp"
 awk 'BEGIN {FS="\t"};\
-	NR==1{$14="Pass/Fail";print;next};\
+	NR==0{$14="Pass/Fail";print;next};\
 	$9 > 42 && $9 < 47.5 && $10 >= 28 && $11 >= 20{printf "%s\t%s\n", $0, "PASS"};\
-	$9 < 42 || $9 > 47.5 || $10 < 28 || $11 < 20{printf "%s\t%s\n", $0, "FAIL"};' $run_dir/output/$1/stats/qc_report/qc_report.txt > $run_dir/output/$1/$1"_QCREPORT.temp"
- 
-awk '(NR>1)' $run_dir/output/$1/$1"_QCREPORT.temp" > $run_dir/output/$1/$1"_QCREPORT2.temp" 
+	$9 < 42 || $9 > 47.5 || $10 < 28 || $11 < 20{printf "%s\t%s\n", $0, "FAIL"};' $run_dir/output/$1/$1"_QCREPORT.temp" > $run_dir/output/$1/$1"_QCREPORT2.temp" 
 
 awk 'BEGIN {FS="\t"};\
-  NR==2
+  NR==0
 	$9 > 42 && $9 < 47.5 && $10 >= 28 && $11 >= 20{printf "%s\t%s\n", $0, " "};\
  	$9 < 42 || $9 > 47.5{printf "%s\t%s\n", $0, "GC_After_Trimming not within 42%-47.5%"};\
  	$10 < 28{printf "%s\t%s\n", $0, "Average_Q_Score_After_Trimming not greater than or equal to 28"};\
-	$11 < 20{printf "%s\t%s\n", $0, "Reference_Length_Coverage_After_Trimming not greater than or equal to 20X"};' $run_dir/output/$1/$1"_QCREPORT2.temp" > $run_dir/output/$1/$1"_QCREPORT3.temp"
+	$11 < 20{printf "%s\t%s\n", $0, "Reference_Length_Coverage_After_Trimming not greater than or equal to 20X"};' $run_dir/output/$1/$1"_QCREPORT2.temp"  > $run_dir/output/$1/$1"_QCREPORT3.temp" 
+ 
+ awk 'BEGIN {FS="\t"};\
+  {print $1,"\t"$9,"\t"$10,"\t"$11,"\t"$14,"\t"$15};' $run_dir/output/$1/$1"_QCREPORT3.temp"  > $run_dir/output/$1/$1"_QCREPORT4.temp" 
 
-echo "Sample_Name	Reads_Before_Trimming	GC_Before_Trimming	Average_Q_Score_Before_Trimming	Reference_Length_Coverage_Before_Trimming	Reads_After_Trimming	Paired_Reads_After_Trimming	Unpaired_Reads_After_Trimming	GC_After_Trimming	Average_Q_Score_After_Trimming	Reference_Length_Coverage_After_Trimming	Mean_Coverage_Depth	Reads_Mapped	Pass/Fail  Comments" | cat - $run_dir/output/$1/$1"_QCREPORT3.temp" > $run_dir/output/$1/$1"_QCREPORT.txt"
-
+echo "Sample_Name	GC_After_Trimming	Average_Q_Score_After_Trimming	Reference_Length_Coverage_After_Trimming	Pass/Fail	QC_Comment" | cat - $run_dir/output/$1/$1"_QCREPORT4.temp" > $run_dir/output/$1/$1"_QC_REPORT.txt"
+#done
 
 rm $run_dir/output/$1/$1"_QCREPORT.temp"
 rm $run_dir/output/$1/$1"_QCREPORT2.temp"
 rm $run_dir/output/$1/$1"_QCREPORT3.temp"
+rm $run_dir/output/$1/$1"_QCREPORT4.temp"
