31e9cefeaf1c04ddc62d2e0ee261020fea08adcf
jessres <127244776+jessres@users.noreply.github.com>
2024-04-04 10:30:36 -0500

    Create submit_to_SRA.sh

---
+++ scripts/submit_to_SRA.sh
@@ -0,0 +1,2 @@
+#/home/jiel/.aspera/cli/bin/ascp -i /home/jiel/ssh/aspera.openssh -QT -l100m -k1 -d /home/dnalab/cecret_runs/$1/SRA_fastq subasp@upload.ncbi.nlm.nih.gov:uploads/lab.microbiology_dshs.state.tx.us_rJiZeQDA/$1
+/home/dnalab/.aspera/connect/bin/ascp -i /home/dnalab/aspera.openssh -QT -l100m -k1 -d /bioinformatics/Candida_auris/mycosnp-nf/output/$1/SRA_fastq/ subasp@upload.ncbi.nlm.nih.gov:uploads/lab.microbiology_dshs.state.tx.us_rJiZeQDA/$1
